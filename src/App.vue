d924a25776b8dbe7bed2f4b4f1d0244314a30fc2
Samuel Magafu <hi@sammagafu.com>
2022-09-11 00:25:22 +0300

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import TopNavigation from '@/components/TopNavigation.vue'
+</script>
+
+<template>
+  <TopNavigation />
+  <RouterView />
+</template>
